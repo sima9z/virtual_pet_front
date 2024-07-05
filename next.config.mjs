99c30af5651c5e42e08d9617b1ec2cc179c800d9
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-05 22:22:31 +0900

    PWA install

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: true,
+  images: {
+    domains: ['cover.openbd.jp'],
+  },
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+  },
+};
+
+export default () => {
+  const plugins = [withPWA];
+  const config = plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig,
+  });
+  return config;
+};
