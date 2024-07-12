92097fb3cf199fcdfbaadb42e0591aec3a57067d
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-12 22:54:08 +0900

    toppage

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme, Container, Box } from '@mui/material';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
-import { Button, Typography } from '@mui/material';
+import Image from 'next/image';
+import { Button } from '@mui/material';
+import { blue } from '@mui/material/colors';
 
 const cache = createCache({ key: 'css', prepend: true });
 const theme = createTheme();
@@ -17,14 +19,33 @@ export default function Home() {
     router.push('/login');
   };
 
+const theme = createTheme({
+    palette: {
+      primary: blue,
+      secondary: {
+        main: '#f8bbd0',
+      },
+  },
+});
+
   return (
     <CacheProvider value={cache}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Typography variant="h1">Welcome to the Virtual Pet App</Typography>
-        <Button variant="contained" color="primary" onClick={goToLogin}>
-          Go to Login
-        </Button>
+        <Container>
+          <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="100vh">
+              <Image
+                src="/ばーちゃるぺっとロゴ.png" // アップロードした画像のパスを指定
+                alt="Virtual Pet App Logo"
+                width= {1000} // 必要に応じて調整
+                height={1000} // 必要に応じて調整
+                className="mx-auto w-7/10 h-auto"
+              />
+            <Button variant="contained" color="secondary" size="large" onClick={goToLogin} sx={{ color: 'white' }}>
+              Login
+            </Button>
+          </Box>
+        </Container>
       </ThemeProvider>
     </CacheProvider>
   );
