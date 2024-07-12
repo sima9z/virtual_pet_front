04b7c0e5db856ef0cc459e101a40cbfb8bb190a3
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-12 23:30:17 +0900

    futter

---
+++ app/components/organisms/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+
+const Footer = () => {
+  return(
+    <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+    </AppBar>
+  )
+}
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/organisms/Footer"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
