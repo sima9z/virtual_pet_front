052a26c74f826869fd4caad91faad94ffafe7645
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-17 04:30:26 +0900

    signupLoginLogout4

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import { config } from 'dotenv';
+
+config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 };
 
 export default nextConfig;
