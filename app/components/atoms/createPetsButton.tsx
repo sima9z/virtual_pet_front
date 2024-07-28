05691c924e6ee0de6b20b14d5d3648d5203eea6a
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-28 23:08:44 +0900

    userID5

--- app/components/atoms/createPetsButton.tsx
+++ app/components/atoms/createPetsButton.tsx
@@ -7,7 +7,7 @@ const cache = createCache({ key: 'css', prepend: true });
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-type CreatePetsButtonProps = {
+interface CreatePetsButtonProps  {
   petName: string;
   selectedPetType: string;
   selectedPetLook: string;
