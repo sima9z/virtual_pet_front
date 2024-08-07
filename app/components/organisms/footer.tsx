6693162c9b4f8b59fcf4717cd12817bc600e2a00
sima9z <159206331+sima9z@users.noreply.github.com>
2024-08-08 00:47:52 +0900

    inquiry

--- app/components/organisms/footer.tsx
+++ app/components/organisms/footer.tsx
@@ -31,6 +31,9 @@ const Footer = () => {
   const goToPreparation2 = () => {
     router.push('https://kiyac.app/privacypolicy/3QB6GsYYlo7FzDYekrOB');
   };
+  const goToPreparation3 = () => {
+    router.push('https://docs.google.com/forms/d/e/1FAIpQLScbwBebtL1O1Oxz2XG5sX_DVSsyzQL6FDzS0gTk1p0jkSJxig/viewform?usp=sf_link');
+  };
 
   return (
     <CacheProvider value={cache}>
@@ -48,6 +51,9 @@ const Footer = () => {
                 </Link>
                 <Link href="#" color="primary" underline="hover" onClick={goToPreparation2}>
                     プライバシーポリシー
+                </Link>
+                <Link href="#" color="primary" underline="hover" onClick={goToPreparation3}>
+                    お問い合わせ
                 </Link>
               </Box>
             </Box>
