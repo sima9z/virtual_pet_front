2bddf420e77ca9c71c5100fa2d565a2a510b89a8
sima9z <159206331+sima9z@users.noreply.github.com>
2024-08-16 22:03:20 +0900

    PuppyDogAnimation2

--- app/components/PuppyDogAnimation.tsx
+++ app/components/PuppyDogAnimation.tsx
@@ -135,10 +135,16 @@ const PuppyDogAnimation = ()=>{
   };
 
   useEffect(() => {
-    if (!isSitting) {
-      const stopWalkingAnimation = startWalkingAnimation();
-      return stopWalkingAnimation;
+    if (containerRef.current) {
+      const randomStartPosition = getRandomPosition(); // ランダムな初期位置を取得
+      gsap.set(containerRef.current, { x: randomStartPosition }); // 初期位置を設定
     }
+
+    startWalkingAnimation(); // 歩行アニメーションを開始
+  }, []);
+
+  useEffect(() => {
+    startWalkingAnimation(); // 歩行アニメーションを開始
   }, [isSitting]);
 
   useEffect(() => {
