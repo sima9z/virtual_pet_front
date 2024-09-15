4921d09967822960efd25b167ec30ded62f2ecc7
sima9z <159206331+sima9z@users.noreply.github.com>
2024-09-15 16:08:18 +0900

    fix:犬のバッドステータス時のアニメーションはひとまず完了

--- app/components/DogAnimation.tsx
+++ app/components/DogAnimation.tsx
@@ -211,6 +211,8 @@ const DogAnimation = forwardRef<DogAnimationHandle, {
       repeat: -1,
       yoyo: true,
     });
+
+    UnhappyOrHungryWalkinganimate();
   
     return () => {
       legAnims.forEach(anim => anim.kill());
@@ -241,6 +243,34 @@ const DogAnimation = forwardRef<DogAnimationHandle, {
             scaleX: directionRef.current, // 反転
             duration: 0.5, // 反転時のdurationを固定
             onComplete: animate // 次のアニメーションを呼び出す
+          });
+        });
+      },
+    });
+  };
+
+  const UnhappyOrHungryWalkinganimate = () => {
+    if (!containerRef.current || isSitting) return;
+
+    const containerWidth = containerRef.current.offsetWidth; //containerRefの幅
+    const viewportWidth = window.innerWidth;
+    const direction = directionRef.current;
+
+    const randomStartPosition = getRandomPosition(); // ランダムな初期位置を取得
+
+    gsap.killTweensOf(containerRef.current); // 既存のアニメーションを停止
+
+    gsap.to(containerRef.current, {
+      x: direction * randomStartPosition, // ランダムな初期位置から開始
+      duration: initialSpeed * 5, // 速度を遅くするためにdurationを増やす
+      ease: 'linear', //等速
+      onComplete: () => {
+        directionRef.current *= -1; // 方向を反転
+        requestAnimationFrame(() => { //パフォーマンス
+          gsap.to(containerRef.current, {
+            scaleX: directionRef.current, // 反転
+            duration: initialSpeed * 7,
+            onComplete:  UnhappyOrHungryWalkinganimate // 次のアニメーションを呼び出す
           });
         });
       },
