b650af2cc9b6e3835d261d081055adb504127b40
sima9z <159206331+sima9z@users.noreply.github.com>
2024-09-18 19:36:39 +0900

    refactor:PuppyDogAnimationの画像のインポートとuseRefの定義を分離

--- components/animations/puppyDog/PuppyDogAnimation.tsx
+++ components/animations/puppyDog/PuppyDogAnimation.tsx
@@ -2,34 +2,39 @@ import React, { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import Image from 'next/image';
 
-import legImageBackRight from '../../../public/ダックス奥後ろ足.png';
-import legImageFrontRight from '../../../public/ダックス奥前足2.png';
-import legImageFrontLeft from '../../../public/ダックス前足.png';
-import legImageBackLeft from '../../../public/ダックス後ろ足.png';
-import tailImage from '../../../public/ダックス尻尾.png';
-import headImageFace from '../../../public/ダックス顔.png';
-import headImageEye from '../../../public/ダックス目.png';
-import bodyImage from '../../../public/ダックス胴体.png';
-import earImage from '../../../public/ダックス耳.png';
-import earImageRight from '../../../public/ダックス奥耳.png';
-import jawImage from '../../../public/ダックス顎.png';
+import { puppyDogImageAssets } from '../../../hooks/components/animations/puppyDog/puppyDogImageAssets';
+import { usePuppyDogRefs } from '../../../hooks/components/animations/puppyDog/usePuppyDogRefs';
 
 import { usePuppyDogWalkingAnimation } from '../../../hooks/components/animations/puppyDog/usePuppyDogWalkingAnimation'
 import { usePuppyDogSitAnimation } from '../../../hooks/components/animations/puppyDog/usePuppyDogSitAnimation'
 
 const PuppyDogAnimation = () => {
-  const legBackLeftRef = useRef<HTMLImageElement | null>(null);
-  const legBackRightRef = useRef<HTMLImageElement | null>(null);
-  const legFrontLeftRef = useRef<HTMLImageElement | null>(null);
-  const legFrontRightRef = useRef<HTMLImageElement | null>(null);
-  const tailRef = useRef<HTMLImageElement | null>(null);
-  const headFaceRef = useRef<HTMLImageElement | null>(null);
-  const headEyeRef = useRef<HTMLImageElement | null>(null);
-  const bodyRef = useRef<HTMLImageElement | null>(null);
-  const earRef = useRef<HTMLImageElement | null>(null);
-  const earRightRef = useRef<HTMLImageElement | null>(null);
-  const jawRef = useRef<HTMLImageElement | null>(null);
-  const containerRef = useRef<HTMLDivElement | null>(null);
+  const {  
+    legImageBackRight,
+    legImageFrontRight,
+    legImageFrontLeft,
+    legImageBackLeft,
+    tailImage,
+    headImageFace,
+    headImageEye,
+    bodyImage,
+    earImage,
+    earImageRight,
+    jawImage } = puppyDogImageAssets;
+
+    const {
+      legBackLeftRef,
+      legBackRightRef,
+      legFrontLeftRef,
+      legFrontRightRef,
+      tailRef,
+      headFaceRef,
+      headEyeRef,
+      bodyRef,
+      earRef,
+      earRightRef,
+      jawRef,
+      containerRef } = usePuppyDogRefs;
 
   const initialSpeed = 3; // 初期速度を設定
   const directionRef = useRef(1); // 移動方向を保持する
@@ -113,231 +118,6 @@ const PuppyDogAnimation = () => {
 
   useEffect(() => {
     startWalkingAnimation(); // 歩行アニメーションを開始
-  }, [isSitting]);
-
-  useEffect(() => {
-    if (!isSitting) return;
-
-    gsap.killTweensOf(containerRef.current); // 移動アニメーションを停止
-    gsap.killTweensOf([legBackLeftRef.current, legBackRightRef.current, legFrontLeftRef.current, legFrontRightRef.current, tailRef.current,headFaceRef.current, headEyeRef.current, bodyRef.current, earRef.current, earRightRef.current, jawRef.current]);
-  
-    // 足と体の状態をリセット
-    gsap.set([legBackLeftRef.current, legBackRightRef.current, legFrontLeftRef.current, legFrontRightRef.current, tailRef.current,headFaceRef.current, headEyeRef.current, bodyRef.current, earRef.current, earRightRef.current, jawRef.current], {
-      rotation: 0,
-      x: 0,
-      y: 0,
-    });
-
-    const tl = gsap.timeline({
-      onComplete: () => {
-        setIsSitting(false);
-        startWalkingAnimation(); // 立ち上がった後に移動アニメーションを再開
-      }
-    });
-
-    // 座るアニメーション
-    tl.to(headFaceRef.current, {
-      y: -40,
-      rotation: -20,
-      transformOrigin: 'bottom',
-      duration: 1.0,
-    });
-    tl.to(
-      [earRef.current, earRightRef.current, jawRef.current],
-      {
-        x: -27,
-        y: -38,
-        rotation: -3,
-        transformOrigin: 'top',
-        duration: 1.0,
-      },
-      '<'
-    );
-    tl.to(
-      headEyeRef.current,
-      {
-        x: -25,
-        y: -30,
-        rotation: -15,
-        transformOrigin: 'bottom',
-        duration: 1.0,
-      },
-      '<'
-    );
-    tl.to(
-      tailRef.current,
-      {
-        y: 50,
-        x: -30,
-        rotation: 10,
-        transformOrigin: 'top',
-        duration: 1.0,
-        ease: 'power3.out',
-      },
-      '<'
-    );
-    tl.to(
-      legBackLeftRef.current,
-      {
-        y: 60,
-        x: -30,
-        rotation: 70,
-        transformOrigin: 'top',
-        duration: 1.0,
-        ease: 'power3.out',
-      },
-      '<'
-    );
-    tl.to(
-      legBackRightRef.current,
-      {
-        x: -80,
-        y: -10,
-        rotation: 30,
-        transformOrigin: 'top',
-        duration: 1.0,
-        ease: 'power3.out',
-      },
-      '<'
-    );
-    tl.to(
-      legFrontLeftRef.current,
-      {
-        x: -100,
-        y: -10,
-        rotation: -40,
-        transformOrigin: 'top',
-        duration: 1.0,
-        ease: 'power3.out',
-      },
-      '<'
-    );
-    tl.to(
-      legFrontRightRef.current,
-      {
-        y: 10,
-        x: -53,
-        rotation: -40,
-        transformOrigin: 'top',
-        duration: 1.0,
-        ease: 'power3.out',
-      },
-      '<'
-    );
-    tl.to(
-      bodyRef.current,
-      {
-        rotation: 30,
-        transformOrigin: 'top',
-        duration: 1.0,
-        ease: 'power3.out',
-      },
-      '<'
-    );
-
-    // 逆再生で立ち上がるアニメーション
-    tl.to(bodyRef.current, {
-      rotation: 0,
-      transformOrigin: 'top',
-      duration: 1.5,
-      ease: 'power3.in',
-    });
-    tl.to(
-      legFrontRightRef.current,
-      {
-        y: 0,
-        x: 0,
-        rotation: 0,
-        transformOrigin: 'top',
-        duration: 1.5,
-        ease: 'power3.in',
-      },
-      '<'
-    );
-    tl.to(
-      legFrontLeftRef.current,
-      {
-        x: 0,
-        y: 0,
-        rotation: 0,
-        transformOrigin: 'top',
-        duration: 1.5,
-        ease: 'power3.in',
-      },
-      '<'
-    );
-    tl.to(
-      legBackRightRef.current,
-      {
-        x: 0,
-        y: 0,
-        rotation: 0,
-        transformOrigin: 'top',
-        duration: 1.5,
-        ease: 'power3.in',
-      },
-      '<'
-    );
-    tl.to(
-      legBackLeftRef.current,
-      {
-        y: 0,
-        x: 0,
-        rotation: 0,
-        transformOrigin: 'top',
-        duration: 1.5,
-        ease: 'power3.in',
-      },
-      '<'
-    );
-    tl.to(
-      tailRef.current,
-      {
-        y: 0,
-        x: 0,
-        rotation: 0,
-        transformOrigin: 'top',
-        duration: 1.5,
-        ease: 'power3.in',
-      },
-      '<'
-    );
-    tl.to(
-      headEyeRef.current,
-      {
-        x: 0,
-        y: 0,
-        rotation: 0,
-        transformOrigin: 'bottom',
-        duration: 3.1,
-      },
-      '<'
-    );
-    tl.to(
-      [earRef.current, earRightRef.current, jawRef.current],
-      {
-        x: 0,
-        y: 0,
-        rotation: 0,
-        transformOrigin: 'top',
-        duration: 3.1,
-      },
-      '<'
-    );
-    tl.to(
-      headFaceRef.current,
-      {
-        y: 0,
-        rotation: 0,
-        transformOrigin: 'bottom',
-        duration: 3.1,
-      },
-      '<'
-    );
-
-    return () => {
-      tl.kill(); //クリーンアップ
-    };
   }, [isSitting]);
 
   const handleClick = () => {
