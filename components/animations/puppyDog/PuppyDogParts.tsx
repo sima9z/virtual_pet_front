5e5ca8401aff884b49b6192a59f308a0706907d1
sima9z <159206331+sima9z@users.noreply.github.com>
2024-09-21 20:02:45 +0900

    chore:子犬と子猫の画像データの読み込みをS3からに変更

--- components/animations/puppyDog/PuppyDogParts.tsx
+++ components/animations/puppyDog/PuppyDogParts.tsx
@@ -41,66 +41,88 @@ const PuppyDogParts = ( { refs }: PuppyDogPartsProps ) => {
         src={legImageBackLeft}
         alt="Back Left Leg"
         className="dog-part absolute top-[170px] left-[300px]"
+        width={122}
+        height={172}
       />
       <Image
         ref={legBackRightRef}
         src={legImageBackRight}
         alt="Back Right Leg"
         className="dog-part absolute top-[220px] left-[220px]"
+        width={94}
+        height={115}
       />
       <Image
         ref={legFrontLeftRef}
         src={legImageFrontLeft}
         alt="Front Left Leg"
         className="dog-part absolute top-[170px] left-[80px]"
+        width={137}
+        height={154}
       />
       <Image
         ref={legFrontRightRef}
         src={legImageFrontRight}
         alt="Front Right Leg"
         className="dog-part absolute top-[120px] left-[10px]"
+        width={137}
+        height={154}
       />
       <Image
         ref={tailRef}
         src={tailImage}
         alt="Tail"
         className="dog-part absolute top-[60px] left-[350px]"
+        width={83}
+        height={127}
       />
       <Image
         ref={headFaceRef}
         src={headImageFace}
         alt="Face"
         className="dog-part absolute top-[5px] left-[60px]"
+        width={167}
+        height={123}
       />
       <Image
         ref={headEyeRef}
         src={headImageEye}
         alt="Eye"
         className="dog-part absolute top-[15px] left-[125px]"
+        width={45}
+        height={42}
       />
       <Image
         ref={bodyRef}
         src={bodyImage}
         alt="Body"
         className="dog-part absolute top-[100px] left-[85px]"
+        width={304}
+        height={166}
       />
       <Image
         ref={earRef}
         src={earImage}
         alt="Ear"
         className="dog-part absolute top-[20px] left-[180px]"
+        width={74}
+        height={113}
       />
       <Image
         ref={earRightRef}
         src={earImageRight}
         alt="EarRight"
         className="dog-part absolute top-[-7px] left-[103px]"
+        width={68}
+        height={32}
       />
       <Image
         ref={jawRef}
         src={jawImage}
         alt="Jaw"
         className="dog-part absolute top-[75px] left-[90px]"
+        width={81}
+        height={29}
       />
     </>
   )
--- hooks/components/animations/puppyCat/puppyCatImageAssets.ts
+++ hooks/components/animations/puppyCat/puppyCatImageAssets.ts
@@ -1,13 +1,16 @@
-import legImageBackRight from '../../../../public/三毛猫奥後足.png';
-import legImageFrontRight from '../../../../public/三毛猫奥前足.png';
-import legImageFrontLeft from '../../../../public/三毛猫手前前足.png';
-import legImageBackLeft from '../../../../public/三毛猫手間後ろ足.png';
-import tailImage from '../../../../public/三毛猫尻尾.png';
-import faceImage from '../../../../public/三毛猫顔.png';
-import bodyImage from '../../../../public/三毛猫胴体.png';
-import earImage from '../../../../public/三毛猫耳.png';
-import beardImageRight from '../../../../public/三毛猫右ひげ.png';
-import beardImageLeft from '../../../../public/三毛猫左ひげ.png';
+const bucketName = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
+const region = process.env.NEXT_PUBLIC_AWS_REGION;
+
+const legImageBackRight = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫奥後足.png`;
+const legImageFrontRight = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫奥前足.png`;
+const legImageFrontLeft = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫手前前足.png`;
+const legImageBackLeft = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫手間後ろ足.png`;
+const tailImage = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫尻尾.png`;
+const faceImage = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫顔.png`;
+const bodyImage = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫胴体.png`;
+const earImage = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫耳.png`;
+const beardImageRight = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫右ひげ.png`;
+const beardImageLeft = `https://${bucketName}.s3.${region}.amazonaws.com/三毛猫/三毛猫左ひげ.png`;
 
 export const puppyCatImageAssets = {
   legImageBackRight,
