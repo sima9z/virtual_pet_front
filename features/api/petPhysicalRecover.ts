b690af1df2f10ffc03151c1565d2e0f7edcb9ff2
sima9z <159206331+sima9z@users.noreply.github.com>
2024-09-24 21:41:31 +0900

    feat:体力がリアルタイムで反映されるように設定

--- features/api/petPhysicalRecover.ts
+++ features/api/petPhysicalRecover.ts
@@ -1,6 +1,9 @@
+import { getPetDetails } from './getPetDetails';
+import { PetDetails } from '../../types/index';
+
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export const petPhysicalRecover = async (): Promise<void> => {
+export const petPhysicalRecover = async (setPetDetails: React.Dispatch<React.SetStateAction<PetDetails | null>>): Promise<void> => {
   try {
     const response = await fetch(`${API_BASE_URL}/pet_physical_recover`, {
       method: 'POST',
@@ -17,6 +20,9 @@ export const petPhysicalRecover = async (): Promise<void> => {
       throw new Error('Failed to recover pet physical');
     }
 
+    // 最新のペット情報を取得して状態を更新
+    const updatedPetDetails = await getPetDetails();
+    setPetDetails(updatedPetDetails);
     console.log('Pet physical recovered successfully');
   } catch (error) {
     console.error('Error recovering pet physical:', error);
