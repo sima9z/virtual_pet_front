8924130b4862efa54d84d29c36d73424cdbe0ae1
sima9z <159206331+sima9z@users.noreply.github.com>
2024-09-28 20:05:00 +0900

    feat:Firebaseの設定情報を環境変数へ保存

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyClF3gsIaJS-goQcTs6F1CNTMfxyu_1nyo",
-  authDomain: "virtualpet-5e946.firebaseapp.com",
-  projectId: "virtualpet-5e946",
-  storageBucket: "virtualpet-5e946.appspot.com",
-  messagingSenderId: "121658404968",
-  appId: "1:121658404968:web:e250082b485a2c08cd8bdd",
-  measurementId: "G-7VNBNEVE27"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Firebaseの初期化
